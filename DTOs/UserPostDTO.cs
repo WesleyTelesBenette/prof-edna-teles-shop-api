2cf0c26197ca337884c26302b3f42383cd1a6f22
Wesley T. Benette <weluonf@gmail.com>
2024-10-22 16:57:41 -0300

    :sparkles: feat(User): Updates Repository.

--- DTOs/UserPostDTO.cs
+++ DTOs/UserPostDTO.cs
@@ -1,22 +1,31 @@
-﻿using prof_edna_teles_shop_api.Models;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace prof_edna_teles_shop_api.DTOs;
 
 public class UserPostDTO
 {
-    [Range(1, long.MaxValue, ErrorMessage = "O Id deve ser um número positivo.")]
-    public long? Id { get; set; } = null;
-
+    [Required(ErrorMessage = "O nome é obrigatório.")]
     [StringLength(100, ErrorMessage = "O nome não pode ter mais de 100 caracteres.")]
-    public string? Name { get; set; } = null;
+    public string Name { get; set; }
 
+    [Required(ErrorMessage = "O e-mail é obrigatório.")]
     [EmailAddress(ErrorMessage = "O e-mail deve ser um endereço válido.")]
     [StringLength(200, ErrorMessage = "O e-mail não pode ter mais de 200 caracteres.")]
-    public string? Email { get; set; } = null;
+    public string Email { get; set; }
 
+    [Required(ErrorMessage = "A senha é obrigatória.")]
     [StringLength(100, MinimumLength = 6, ErrorMessage = "A senha deve ter entre 6 e 100 caracteres.")]
-    public string? Password { get; set; } = null;
+    public string Password { get; set; }
 
-    public ICollection<Product>? Products { get; set; } = null;
+    public ICollection<long>? ProductsId { get; set; } = [];
+
+    public UserPostDTO() {}
+
+    public UserPostDTO(string name, string email, string password, ICollection<long> productsId)
+    {
+        Name = name;
+        Email = email;
+        Password = password;
+        ProductsId = productsId;
+    }
 }
