dc0e2f580b4bdbd65160f4a131554aba0e99fffa
Wesley T. Benette <weluonf@gmail.com>
2024-10-24 11:38:46 -0300

    :sparkles: feat: Creates Category DTOs.

---
+++ DTOs/CategorytPutDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace prof_edna_teles_shop_api.DTOs;
+
+public class CategoryPutDTO
+{
+    [Key]
+    [Required(ErrorMessage = "O Id é obrigatório.")]
+    [Range(1, long.MaxValue, ErrorMessage = "O Id deve ser um número positivo.")]
+    public long Id { get; set; }
+
+    [StringLength(100, ErrorMessage = "O nome não pode ter mais de 100 caracteres.")]
+    public string Name { get; set; }
+
+    [Url(ErrorMessage = "A URL da imagem deve ser válida.")]
+    public string ImageUrl { get; set; }
+}
