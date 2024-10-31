dee7e85a27dbef9ce870a24313ca342adc188874
Wesley T. Benette <weluonf@gmail.com>
2024-10-31 10:39:23 -0300

    :sparkles: feat: Creates Controller for testing.

---
+++ Controllers/TestController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace prof_edna_teles_shop_api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TestController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetTest()
+    {
+        return Ok("A API funciona.");
+    }
+}
